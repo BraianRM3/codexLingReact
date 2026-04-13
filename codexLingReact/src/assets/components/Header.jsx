2807272c0dc7a534798072800cfb82800ddb64d2
brahyan.rincon <braianrincon1@gmail.com>
2026-04-13 12:50:11 -0500

    Agregue nuevos componentes al home y al header

--- codexLingReact/src/assets/components/Header.jsx
+++ codexLingReact/src/assets/components/Header.jsx
@@ -1,19 +1,37 @@
 import logo from "../Images/logocodex.png";
-function Header({ setPage, toggleMenu }) {
+function Header({ setPage, toggleMenu, search, setSearch, showSearch, setShowSearch }) {
+   const handleHomeClick = () => {
+    setPage("home");
+    setSearch("");       // 🔥 Limpia búsqueda
+    setShowSearch(false); // 🔥 Cierra buscador
+  };
   return (
     <header>
-      <div className="logo" onClick={() => setPage("home")}>
+      <div className="logo" onClick={handleHomeClick}>
         <div className="logo-icon">
             <img src={logo} alt="CodexLing Logo" />
         </div>
         <div className="logo-text">CODEXLING</div>
       </div>
-
       <div className="nav-icons">
-        <div className="nav-icon">
+      {/* INPUT QUE APARECE */}
+      <div className="search-container">
+        {showSearch && (
+          <input
+            type="text"
+            placeholder="Buscar..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="search-header"
+          />
+        )}
+        </div>
+        {/* 🔍 BUSCADOR */}
+        <div className="nav-icon" onClick={() => setShowSearch(!showSearch)}>
           <i className="fas fa-search"></i>
         </div>
-
+        
+        
         <div className="nav-icon">
           <i className="fas fa-user"></i>
         </div>
